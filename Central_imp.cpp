a7df11daa0efa9bbe8f86bfdd43fbb5119d91739
JoeLaible123 <165977540+JoeLaible123@users.noreply.github.com>
2025-09-25 16:10:44 -0400

    Clean up comments in Central_imp.cpp
    Removed outdated comments and references.

--- Central_imp.cpp
+++ Central_imp.cpp
@@ -1,10 +1,4 @@
 //Joseph I Laible
-// Professor Jonathan S. Weissman
-// Cis 554 Object Oriented Programming in C++
-// Syracuse University 
-// text ref - C++ How to program 10th ed, Paul & Harvey Deitel 
-//Final project
-//3/14/2024
 // This is the  ".cpp" implemenation file  for derived  class Central_powers
 #include "Central.h"
 #include <iostream>
